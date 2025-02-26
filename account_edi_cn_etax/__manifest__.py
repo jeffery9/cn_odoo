e087ced57c9952e80dcf538fde391b067b62a2ff
jeffery <jeffery9@gmail.com>
2025-02-26 10:13:42 +0000

    add edi

---
+++ account_edi_cn_etax/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "EDI: china etax",
+    "summary": """
+    EDI: china etax
+
+        """,
+    "description": """
+
+     
+
+    """,
+    "author": "genin IT, 亘盈信息技术, jeffery <jeffery9@gmail.com>",
+    "website": "http://www.geninit.cn",
+    "category": "Utility",
+    "version": "0.1",
+    "depends": ["l10n_cn_tax"],
+    "external_dependencies": {
+        "python": [ ],
+    },
+    # always loaded
+    "data": [
+        
+    ],
+    "application": True,
+    
+    "license": "AGPL-3",
+}
