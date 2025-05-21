7607e7334a81cf1fb819db7a967875126827d1f3
jeffery <jeffery9@gmail.com>
2025-05-21 13:53:58 +0800

    add city data

---
+++ l10n_cn_city/__manifest__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Copyright (C) 2018-now  jeffery9@gmail.com
+
+{
+    'name': 'China - City Data',
+    'countries': ['cn'],
+    'version': '1.8',
+    'category': 'Accounting/Localizations',
+    'author': 'Jeffery Chen Fan<jeffery9@gmail.com>',
+    'description': """
+Includes the following data for the Chinese localization
+========================================================
+
+City Data/城市数据
+
+    """,
+    'depends': ['l10n_cn', 'base_address_extended'],
+    'data': [
+        'data/res_country_data.xml',
+        "data/res_city_data_ah.xml",
+        "data/res_city_data_bj.xml",
+        "data/res_city_data_cq.xml",
+        "data/res_city_data_fj.xml",
+        "data/res_city_data_gd.xml",
+        "data/res_city_data_gs.xml",
+        "data/res_city_data_gx.xml",
+        "data/res_city_data_gz.xml",
+        "data/res_city_data_ha.xml",
+        "data/res_city_data_hb.xml",
+        "data/res_city_data_he.xml",
+        "data/res_city_data_hi.xml",
+        "data/res_city_data_hl.xml",
+        "data/res_city_data_hn.xml",
+        "data/res_city_data_jl.xml",
+        "data/res_city_data_js.xml",
+        "data/res_city_data_jx.xml",
+        "data/res_city_data_ln.xml",
+        "data/res_city_data_nm.xml",
+        "data/res_city_data_nx.xml",
+        "data/res_city_data_qh.xml",
+        "data/res_city_data_sc.xml",
+        "data/res_city_data_sd.xml",
+        "data/res_city_data_sh.xml",
+        "data/res_city_data_sn.xml",
+        "data/res_city_data_sx.xml",
+        "data/res_city_data_tj.xml",
+        "data/res_city_data_xj.xml",
+        "data/res_city_data_xz.xml",
+        "data/res_city_data_yn.xml",
+        "data/res_city_data_zj.xml",
+    ],
+    'license': 'LGPL-3',
+}
